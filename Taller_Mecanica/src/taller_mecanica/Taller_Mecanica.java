a1b897b0a22973514e351b93423283d6dd8b4823
joancayala <jflaco85@gmail.com>
2023-11-24 02:08:06 -0500

    Changes on Taller_Mecanica

--- Taller_Mecanica/src/taller_mecanica/Taller_Mecanica.java
+++ Taller_Mecanica/src/taller_mecanica/Taller_Mecanica.java
@@ -7,7 +7,10 @@ public class Taller_Mecanica {
     
     public static void main(String[] args) {
        
-        Ventana y = new Ventana();
+        //Ventana y = new Ventana();
+        //y.setVisible(true);
+        javax.swing.JTextArea txtPro = new javax.swing.JTextArea();
+        Ventana y = new Ventana(txtPro);
         y.setVisible(true);
         
     }
